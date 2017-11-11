1877b7bf3aea9daa66227eac51e6330c5817492d
Christopher <christopher.unifesp@gmail.com>
2017-11-10 23:58:27 -0200

    Comment blocks of code

--- autoSarima.py
+++ autoSarima.py
@@ -16,6 +16,9 @@ import math
 
 plt.style.use('fivethirtyeight')
 
+######################################################################
+#                       Import Data
+######################################################################
 co2Data = sm.datasets.co2.load_pandas()
 y = co2Data.data
 
@@ -25,10 +28,13 @@ y = y['co2'].resample('MS').mean()
 # The term bfill means that we use the value before filling in missing values
 y = y.fillna(y.bfill())
 
-print(y)
-
 y.plot(figsize=(15, 6))
 plt.show()
+
+
+######################################################################
+#                       Fitting Model
+######################################################################
 
 # Define the p, d and q parameters to take any value between 0 and 2
 p = d = q = range(0, 2)
@@ -55,6 +61,11 @@ for param in pdq:
             print('ARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, results.aic))
         except:
             continue
+
+
+######################################################################
+#                       Residual Analysis
+######################################################################
         
 mod = sm.tsa.statespace.SARIMAX(y,
                                 order=(1, 1, 1),
@@ -64,35 +75,12 @@ mod = sm.tsa.statespace.SARIMAX(y,
 
 results = mod.fit()
 
-print(results.summary().tables[1])
-
 results.plot_diagnostics(figsize=(15, 12))
 plt.show()
-#
-##The dynamic=False argument ensures that we produce one-step ahead forecasts, meaning that forecasts
-## at each point are generated using the full history up to that point.
-#pred = results.get_prediction(start=pd.to_datetime('1998-01-01'), dynamic=False)
-#pred_ci = pred.conf_int()
-#
-#ax = y['1990':].plot(label='observed')
-#pred.predicted_mean.plot(ax=ax, label='One-step ahead Forecast', alpha=.7)
-#
-#ax.fill_between(pred_ci.index,
-#                pred_ci.iloc[:, 0],
-#                pred_ci.iloc[:, 1], color='k', alpha=.2)
-#
-#ax.set_xlabel('Date')
-#ax.set_ylabel('CO2 Levels')
-#plt.legend()
-#
-#plt.show()
-#
-#y_forecasted = pred.predicted_mean
-#y_truth = y['1998-01-01':]
-#
-## Compute the mean square error
-#rmse = math.sqrt(((y_forecasted - y_truth) ** 2).mean())
-#print('The Root Mean Squared Error of our forecasts is {}'.format(round(rmse, 2)))        
+
+######################################################################
+#                       Predicted x Target
+######################################################################
 
 pred_dynamic = results.get_prediction(start=pd.to_datetime('1998-01-01'), dynamic=True, full_results=True)
 pred_dynamic_ci = pred_dynamic.conf_int()
@@ -113,6 +101,10 @@ ax.set_ylabel('CO2 Levels')
 plt.legend()
 plt.show()
 
+######################################################################
+#                              RMSE
+######################################################################
+
 # Extract the predicted and true values of our time series
 y_forecasted = pred_dynamic.predicted_mean
 y_truth = y['1998-01-01':]
@@ -120,6 +112,11 @@ y_truth = y['1998-01-01':]
 # Compute the mean square error
 rmse = math.sqrt(((y_forecasted - y_truth) ** 2).mean())
 print('The Root Mean Squared Error of our forecasts is {}'.format(round(rmse, 2)))
+
+
+######################################################################
+#                            Forecast
+######################################################################
 
 # Get forecast 500 steps ahead in future
 pred_uc = results.get_forecast(steps=500)
